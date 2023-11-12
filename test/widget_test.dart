e129396325bd76569e0cd7afb07946501792715b
manyasi06 <10466887+manyasi06@users.noreply.github.com>
2023-11-12 14:16:56 -0500

    dummy tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,26 +8,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:ocean_change/app.dart';
+import 'package:ocean_change/screens/login_screen.dart';
 
  // TODO this test is broken
 
-// void main() {
+void main() {
 
+  testWidgets("TestLoginWidget", (widgetTester) async  {
+
+    await widgetTester.pumpWidget(const MaterialApp(
+      home: Material(
+        child : LoginScreen()
+      )
+    ));
+
+    final title = find.text('Ocean Change');
+    expect(title.description, 'text "Ocean Change"');
+
+  });
   
-//   // testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-//   //   // Build our app and trigger a frame.
-//   //   await tester.pumpWidget(const App());
+  // testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  //   // Build our app and trigger a frame.
+  //   await tester.pumpWidget(const App());
 
-//   //   // Verify that our counter starts at 0.
-//   //   expect(find.text('0'), findsOneWidget);
-//   //   expect(find.text('1'), findsNothing);
+  //   // Verify that our counter starts at 0.
+  //   expect(find.text('0'), findsOneWidget);
+  //   expect(find.text('1'), findsNothing);
 
-//   //   // Tap the '+' icon and trigger a frame.
-//   //   await tester.tap(find.byIcon(Icons.add));
-//   //   await tester.pump();
+  //   // Tap the '+' icon and trigger a frame.
+  //   await tester.tap(find.byIcon(Icons.add));
+  //   await tester.pump();
 
-//   //   // Verify that our counter has incremented.
-//   //   expect(find.text('0'), findsNothing);
-//   //   expect(find.text('1'), findsOneWidget);
-//   // });
-// }
+  //   // Verify that our counter has incremented.
+  //   expect(find.text('0'), findsNothing);
+  //   expect(find.text('1'), findsOneWidget);
+  // });
+}
